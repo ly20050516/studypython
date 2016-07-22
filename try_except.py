250bf457902ea5c0cb2d8fc0bddb1e9f4a468e63
LY <daniel1946@163.com>
2016-07-23 02:11:36 +0800

    python3 中 raw_input 变成了 input

--- try_except.py
+++ try_except.py
@@ -2,11 +2,13 @@
 # Filename:try_except.py
 
 import sys
+
 try:
-    s = raw_input('Enter something-->')
+    s = input('Enter something -->')
 except EOFError:
     print('\nWhy did you do an EOF on me?')
     sys.exit()
 except:
     print('\nSome error/exception occurred')
+    
 print('Done')
