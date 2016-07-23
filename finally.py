8504091d6a233495db89ba40734c95cc5a4dc745
LY <daniel1946@163.com>
2016-07-23 18:57:11 +0800

    暂时只能用嵌套两层try except的方式来解决，网上查到有with方式，但不是这里练习的目的。不过其实这个也未达到练习的目的

--- finally.py
+++ finally.py
@@ -3,15 +3,19 @@
 
 import time
 
-global f
 try:
-    f = open('poem.txt')
-    while True:
-        line = f.readline()
-        if len(line) == 0:
-            break
-        time.sleep(2)
-        print(line)
-finally:
-    f.close()
-    print('Cleaning up...closed the file')
+    f = open('poem.txt','r')
+    try:
+        while True:
+            line = f.readline()
+            if(len(line) == 0):
+                break
+            time.sleep(2)
+            print(line)
+    except:
+        print('read file error')
+    finally:
+        f.close()
+        print('cleanup file')
+except FileNotFoundError:
+    print('open file error')
